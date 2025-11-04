adeb7a1a0f38c2e0abe6eaf88d5d2118445be743
Mouss39 <mohamad_almoussa@hotmail.com>
2025-11-04 02:25:45 +0100

    added padding

--- app/src/main/java/mo/show/androidapplication/ui/screens/MainScreen.kt
+++ app/src/main/java/mo/show/androidapplication/ui/screens/MainScreen.kt
@@ -31,7 +31,7 @@ fun MainScreenContent(
         }
     ) {padding ->
 
-        Spacer(Modifier.padding(10.dp))
+        Spacer(Modifier.padding(12.dp))
         Box(
             modifier = Modifier
                 .padding(padding)
